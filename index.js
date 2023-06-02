10e825427c776bb7eeaf26f170947afe8b23bb90
MITE4ETO <dimitrstefanov7v2@gmail.com>
2023-06-02 19:48:02 +0300

    functions are objects

--- index.js
+++ index.js
@@ -1,16 +1,3 @@
-// Factory function
-function createCircle(radius) {
-  return {
-    radius,
-    draw: function () {
-      console.log("draw");
-    },
-  };
-}
-
-const circle = createCircle(1); 
-
-// Constructor function
 function Circle(radius) {
   this.radius = radius;
   this.draw = function () {
@@ -18,4 +5,10 @@ function Circle(radius) {
   };
 }
 
+Circle.call({}, 1);
+Circle.apply({}, [1, 2, 3]);
+
 const another = new Circle(1);
+
+
+
