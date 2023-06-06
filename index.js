3b50de130d268f8216c16668d48f04f96a39203d
MITE4ETO <dimitrstefanov7v2@gmail.com>
2023-06-06 08:51:51 +0300

    multi-level inheritance

--- index.js
+++ index.js
@@ -1,29 +1,12 @@
-// myObj.__proto__ (parent of myObj)
-// Constructor.prototype (parent of myObj )
 
 function Circle(radius) {
-    // Instance members
   this.radius = radius;
 
-  this.move = function() {
-    console.log("move");
-  }
-
-}
-
-const c1 = new Circle(1);
-
-// Prototype members
-Circle.prototype.draw = function() {
+  this.draw = function () {
     console.log("draw");
+  };
 }
 
-c1.draw();
+const circle = new Circle(10);
 
-
-// Returns instance members
-console.log(Object.keys(c1));
-
-
-// Returns all members (instance + prototype)
-for (let key in c1) console.log(key);
+// multi-level inheritence in the console
