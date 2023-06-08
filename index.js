8cec253314d588df68ceca957d57d651f55c89f6
MITE4ETO <dimitrstefanov7v2@gmail.com>
2023-06-08 15:28:32 +0300

    oop

--- index.js
+++ index.js
@@ -1,27 +1,30 @@
 
+
 function Circle(radius) {
   // Instance members
   this.radius = radius;
 
-  this.move = function() {
+  this.move = function () {
     this.draw();
     console.log("move");
-  }
-
+  };
 }
-
 
 // Prototype members
-Circle.prototype.draw = function() {
-  
+Circle.prototype.draw = function () {
   console.log("draw");
-}
+};
 
-const c1  = new Circle(1);
-const c2  = new Circle(1);
-
+const c1 = new Circle(1);
+const c2 = new Circle(1);
 
 // Modifying the toString method
-Circle.prototype.toString = function() {
-  return "Circle with radius " + this.radius; 
-}
+Circle.prototype.toString = function () {
+  return "Circle with radius " + this.radius;
+};
+
+console.log(Object.keys(c1));
+
+
+// Returns all members ( instance + protoype )
+for (let key in c1) console.log(key);
