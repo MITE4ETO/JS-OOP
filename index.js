4c15c13f22b67431486761b44f601668fad5eb56
MITE4ETO <dimitrstefanov7v2@gmail.com>
2023-06-11 14:15:29 +0300

    more oop

--- index.js
+++ index.js
@@ -19,8 +19,24 @@ function HtmlSelectElement(items = []) {
   this.removeItem = function (item) {
     this.items.splice(this.items.indexOf(item), 1);
   };
+
+  this.render = function () {
+    return ` <select>
+    ${this.items.map((item) => `<option>${item}</option>`).join("")}
+    </select>`;
+  };
 }
 
-// baseHtmlSelectElement 
-HtmlSelectElement.prototype = new HtmlElement(); 
+// baseHtmlSelectElement
+HtmlSelectElement.prototype = new HtmlElement();
 HtmlSelectElement.prototype.constructor = HtmlSelectElement;
+
+function HtmlImageElement(src) {
+  this.src = src;
+
+  this.render = function() {
+    return `<img src="${this.src}" />`
+  }
+}
+HtmlImageElement.prototype = new HtmlElement();
+HtmlImageElement.prototype.constructor = HtmlImageElement;
