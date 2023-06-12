1d899d80dbb29bf1142182a02756fe5fd123f315
MITE4ETO <dimitrstefanov7v2@gmail.com>
2023-06-12 15:08:10 +0300

    private members with _name and Symbol();

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
-"use strict";
+
+const _radius = Symbol();
+const _draw = Symbol();
 
 class Circle {
-    draw() {
-        console.log(this);
+    constructor(radius) {
+        this[_radius] = radius;
+    }
+
+    [_draw]() {
+
     }
 }
 
-const c = new Circle();
-const draw = c.draw;
-draw();
+const c = new Circle(1);
+
+
