d64a5b3ac6f8cc8256fd58b610d61a937e1bf4dd
MITE4ETO <dimitrstefanov7v2@gmail.com>
2023-06-13 08:48:33 +0300

    stack exercise

--- index.js
+++ index.js
@@ -1,15 +1,34 @@
 
-class Shape {
-    move() {
-        console.log("moves");
+const _items = new WeakMap();
+
+class Stack {
+    constructor() {
+        _items.set(this, []);
+    }
+
+    push(obj) {
+        _items.get(this).push(obj);
+    }
+
+    pop(){
+        const items = _items.get(this)
+
+        if (items.length === 0)
+        throw new Error("Stack is empty");
+
+        return items.pop();
+    }
+
+    peek() {
+        const items = _items.get(this);
+
+        if(items.length === 0)
+        throw new Error("Stack is empty");
+
+        return items[items.length - 1];
+    }
+
+    get count(){
+        return _items.get(this).length;
     }
 }
-
-class Circle extends Shape {
-  move() {
-    super.move();
-    console.log("circle move");
-  }
-}
-
-const c = new Circle();
