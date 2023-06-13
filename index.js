0f5416125ef2394db1fa617cec0e5c306dd975ed
MITE4ETO <dimitrstefanov7v2@gmail.com>
2023-06-13 10:58:51 +0300

    es6 modules

--- index.js
+++ index.js
@@ -1,34 +1,5 @@
 
-const _items = new WeakMap();
+import { Circle } from "./circle.js";
 
-class Stack {
-    constructor() {
-        _items.set(this, []);
-    }
-
-    push(obj) {
-        _items.get(this).push(obj);
-    }
-
-    pop(){
-        const items = _items.get(this)
-
-        if (items.length === 0)
-        throw new Error("Stack is empty");
-
-        return items.pop();
-    }
-
-    peek() {
-        const items = _items.get(this);
-
-        if(items.length === 0)
-        throw new Error("Stack is empty");
-
-        return items[items.length - 1];
-    }
-
-    get count(){
-        return _items.get(this).length;
-    }
-}
+const c = new Circle(10);
+c.draw();
